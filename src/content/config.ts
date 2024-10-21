66d838317cb3a89455312af366c80a3642b04b5f
Sam <sam618.xu@gmail.com>
2024-10-21 22:31:47 +0800

    (no commit message provided)

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,21 +1,68 @@
-// 从 `astro:content` 导入辅助工具
-import { z, defineCollection } from "astro:content";
-// 为每一个集合定义一个 `type` 和 `schema`
-const postsCollection = defineCollection({
-    type: 'content',
-    schema: z.object({
-      title: z.string(),
-      pubDate: z.date(),
+import { z, defineCollection } from 'astro:content';
+
+const metadataDefinition = () =>
+  z
+    .object({
+      title: z.string().optional(),
+      ignoreTitleTemplate: z.boolean().optional(),
+
+      canonical: z.string().url().optional(),
+
+      robots: z
+        .object({
+          index: z.boolean().optional(),
+          follow: z.boolean().optional(),
+        })
+        .optional(),
+
       description: z.string().optional(),
-      author: z.string(),
-      image: z.object({
-        url: z.string(),
-        alt: z.string()
-      }).optional(),
-      tags: z.array(z.string())
+
+      openGraph: z
+        .object({
+          url: z.string().optional(),
+          siteName: z.string().optional(),
+          images: z
+            .array(
+              z.object({
+                url: z.string(),
+                width: z.number().optional(),
+                height: z.number().optional(),
+              })
+            )
+            .optional(),
+          locale: z.string().optional(),
+          type: z.string().optional(),
+        })
+        .optional(),
+
+      twitter: z
+        .object({
+          handle: z.string().optional(),
+          site: z.string().optional(),
+          cardType: z.string().optional(),
+        })
+        .optional(),
     })
+    .optional();
+
+const postCollection = defineCollection({
+  schema: z.object({
+    publishDate: z.date().optional(),
+    updateDate: z.date().optional(),
+    draft: z.boolean().optional(),
+
+    title: z.string(),
+    excerpt: z.string().optional(),
+    image: z.string().optional(),
+
+    category: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+    author: z.string().optional(),
+
+    metadata: metadataDefinition(),
+  }),
 });
-// 导出一个单独的 `collections` 对象来注册你的集合
+
 export const collections = {
-  posts: postsCollection,
+  post: postCollection,
 };
